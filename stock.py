2d0c9935897fa88aabde7523aaa6b004b05eba45
kouhei0128 <kouhei1412@outlook.jp>
2023-06-28 21:53:03 +0900

    fixed tab2 bug

--- stock.py
+++ stock.py
@@ -26,12 +26,12 @@ def main():
     add_comparison_col = addComparisonCol(new_index_data)
     # excelへ保存
     writeDataToCsv(ticker_symbol, com_name, add_comparison_col)
-    # 前日比算出
     # カレントディレクトリパス取得
     current_dir_path = os.getcwd()
     # カレントディレクトリにフォルダ作成
     folder_path = current_dir_path + "\\{}".format(com_name)
     file_path = folder_path + '\\data.xlsx'
+    # 前日比算出
     get_file_data = valComparison(file_path)
     # 前日比未入力セルの削除
     delNotEnteredCompareCol(file_path)
@@ -104,7 +104,7 @@ def writeDataToCsv(code, com_name, csv_data):
             max_row = sheet.max_row
             # 最終行に追記
             csv_data.to_excel(writer, sheet_name='Sheet', startrow=max_row, header=False)
-        showInfo("データを保存しました")
+        showInfo("データを取得しました")
     except Exception as e:
         showInfo(e)
         
@@ -218,6 +218,9 @@ def saveCodeList():
     can_save_message = '保存しました'
     showInfo(can_save_message)
     
+# コードリストから株価を取得する
+def getDataFromCodeList():
+    pass
 
 # JSONファイルに保存されているコードを読み込み、表示する
 def showCodeSetTable(window):
@@ -240,7 +243,7 @@ def showCodeSetTable(window):
                 # tableに値の挿入
                 tree.insert(parent='', index='end', values=(k, v))
     except FileNotFoundError:
-        return tk.Label(text='コードが保存されていません').place(x=30, y=230)
+        return tk.Label(text='コードが保存されていません')
     except Exception as e:
         print(e)
     return tree
@@ -278,7 +281,7 @@ tab2_code_textbox.place(x=30, y=90)
 # tab2 保存ボタン
 tab2_save_button = tk.Button(tab_two, text='保存ボタン', command=saveCodeList).place(x=30, y=120)
 # tab2 取得ボタン
-tab2_button = tk.Button(tab_two, text='取得').place(x=30, y=150)
+tab2_button = tk.Button(tab_two, text='取得', command=getDataFromCodeList).place(x=30, y=150)
 # tab2 保存済みコード表示テキストラベル
 set_code_label = tk.Label(tab_two, text='保存済みのコード')
 set_code_label.place(x=30, y=200)
